260acb9ee1ad38862bb4400bfc47e7ab1e02f619
phatluu <lhphat.02@gmail.com>
2022-10-11 20:24:55 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+  "./src/**/*.{html,js}",
+  '*.{html,js}'
+  ],
+
   theme: {
     extend: {},
   },
+  
   plugins: [],
 }
